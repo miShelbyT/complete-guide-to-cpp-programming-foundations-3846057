ae83d73dfb7f054ea41ef57d9b8acbde537fe28d
Shelby Talbert <mi.shelbyrose@gmail.com>
2025-07-02 20:59:51 +0000

    exploring variables in C++

--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -4,8 +4,36 @@
 
 #include <iostream>
 
+int a, b = 5; // global variables
+
 int main(){
-    
+    bool my_flag;
+    a = 7;
+    my_flag = false; // will evaluate to 0
+
+    if(my_flag) {
+        std::cout << "first try: " << std::endl;
+        std::cout << "a = " << a << std::endl;
+        std::cout << "b = " << b << std::endl;
+        std::cout << "flag = " << my_flag << std::endl;
+    }
+
+    b = 100;
+    my_flag = true; // will evaluate to 1
+    if(my_flag) {
+        std::cout << "second try: " << std::endl;
+        std::cout << "a = " << a << std::endl;
+        std::cout << "b = " << b << std::endl;
+        std::cout << "flag = " << my_flag << std::endl;
+    }
+
+    unsigned int positive;
+    std::cout << "Type a positive number: " << std::flush;
+    std::cin >> positive;
+    std::cout << "Your number is always positive: " << positive << std::endl;
+    // assigning a negative number to an unsigned integer value will NOT assign the absolute value, but some form of binary conversion.
+
+
     std::cout << std::endl << std::endl;
     return 0;
 }
