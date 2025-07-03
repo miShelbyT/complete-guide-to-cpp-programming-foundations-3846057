4f1b07dc0bc4915163a2915d7d2828240f0e211e
Shelby Talbert <mi.shelbyrose@gmail.com>
2025-07-03 16:07:33 +0000

    working with enum classes

--- src/Ch02/02_09b/CodeDemo.cpp
+++ src/Ch02/02_09b/CodeDemo.cpp
@@ -7,7 +7,20 @@
 
 enum class character_role {protagonist, antagonist, sidekick, npc};
 
+struct game_character{
+    std::string name;
+    int level;
+    character_role role;
+};
+
 int main(){
+
+    game_character peach;
+    peach.name = "Princess Peach"; 
+    peach.level = 5;
+    peach.role = character_role::protagonist;
+
+    std::cout << "New character: " << peach.name << " is a level " << peach.level << " with a role of " << (int) peach.role << std::endl;
     
     std::cout << std::endl << std::endl;
     return 0;
