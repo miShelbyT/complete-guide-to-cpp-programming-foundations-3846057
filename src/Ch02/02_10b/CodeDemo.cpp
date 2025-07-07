a14d8cfca141f5d4fa3e58f1d10db1ddcaa6535e
Shelby Talbert <mi.shelbyrose@gmail.com>
2025-07-07 19:02:34 +0000

    working with number datatypes and operators

--- src/Ch02/02_10b/CodeDemo.cpp
+++ src/Ch02/02_10b/CodeDemo.cpp
@@ -6,7 +6,21 @@
 #include <cstdint>
 
 int main(){
-    
+    float target_x;
+    int32_t sprite_x;
+    uint32_t player_x;
+
+    target_x = -135.45;
+    sprite_x = target_x;
+    player_x = sprite_x;
+
+    std::cout << "Target X (float): " << target_x << std::endl;
+    std::cout << "Sprite X (int32_t): " << sprite_x << std::endl;
+    std::cout << "Player X (uint32_t - not what we expect): " << player_x << std::endl;
+    // binary representation is (2 ^ 32) - intended value
+    std::cout << "Player X (uint32_t): " << static_cast<int32_t>(player_x)<< std::endl;
+    // this converts to int / negative number but not unsigned because the number was negative and can't be cast to unsigned int
+
     std::cout << std::endl << std::endl;
     return 0;
 }
--- src/Ch03/03_05b/CodeDemo.cpp
+++ src/Ch03/03_05b/CodeDemo.cpp
@@ -12,14 +12,35 @@ int main(){
     std::cout << "Initial Zombies Incoming: " << incoming_zombies << std::endl;
 
     // Addition assignment
+    ammo += 10; // 40
+    std::cout << "Ammo after finding more ammo: " << ammo << std::endl;
 
     // Subtraction assignment
+    ammo -= 10; //30
+    std::cout << "Ammo after shooting zombies: " << ammo << std::endl;
+
+    incoming_zombies -= 5; //15
+    std::cout << "Zombies after shooting zombies: " << incoming_zombies << std::endl;
 
     // Multiplication assignment
+    incoming_zombies *= 2; //30
+    std::cout << "Zombies keep coming: " << incoming_zombies << std::endl;
 
     // Division assignment
+    incoming_zombies /= 3; //10
+    std::cout << "Shooting zombies through zombies: " << incoming_zombies << std::endl;
+
+    ammo -= 3; //27
+    std::cout << "Ammo after shooting zombies through zombies: " << ammo << std::endl;
 
     // Remainder assignment
+    std::cout << "Final zombie count should be 10: " << incoming_zombies << std::endl;
+
+    std::cout << "Final ammo count should be 27: " << ammo << std::endl;
+
+    ammo %= 10;
+    std::cout << "If each magazine holds 10 rounds the remainder should be 7: " << ammo << std::endl;
+
 
     std::cout << std::endl << std::endl;
     return 0;
