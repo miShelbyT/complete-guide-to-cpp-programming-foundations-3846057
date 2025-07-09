1addb3c34ac2547834415733073a32e61073ea72
Shelby Talbert <mi.shelbyrose@gmail.com>
2025-07-09 17:50:08 +0000

    working with cstring and string classes

--- src/Ch04/04_03b/CodeDemo.cpp
+++ src/Ch04/04_03b/CodeDemo.cpp
@@ -8,7 +8,14 @@ int main(){
     int highScores[] = {850, 745, 1220, 990};  // High scores in various game stages
     int* scorePtr = highScores;   // Pointer initialized to the address of the array
 
-    std::cout << "Initial high score: " << *scorePtr << std::endl;  // Display the first high score using pointer
+    // highScores = {1, 2, 3, 4}; cannot modify value of highScores variable after it is declared
+
+    std::cout << "Initial high score: " << *scorePtr << std::endl;  // Display the first high score using pointer. points to place in memory
+    
+    std::cout << "Second high score: " << *(scorePtr + 1) << std::endl;  // Display the SECOND high score using initial pointer reference
+
+    // ++scorePtr; will change the value of the pointer
+    std::cout << "Third high score: " << scorePtr[2] << std::endl;  // Display the THIRD high score using initial pointer reference
 
     std::cout << std::endl << std::endl;
     return 0;
--- src/Ch04/04_09b/CodeDemo.cpp
+++ src/Ch04/04_09b/CodeDemo.cpp
@@ -5,14 +5,29 @@
 #include <iostream>
 #include <cstring>
 
+
 int main(){
     const size_t LENGTH = 50;
-    char racer1[];
+    char racer1[] = "Speedy";
     char racer2[LENGTH];
     char raceResult[LENGTH * 2];
 
+    strncpy(racer2, "Lightning McQueen", sizeof(racer2)-1); //taking racer2, copying the second argument into this char array and specifying its length as length-1 to account for terminating character)
+    racer2[sizeof(racer2)-1] = '\0'; // this explicitly sets the final character in this char array as null which is '\0'
+
     std::cout << "Racer 1: " << racer1 << std::endl;
     std::cout << "Racer 2: " << racer2 << std::endl;
+
+    strncpy(raceResult, "And the winner is..... ", sizeof(raceResult)-1);
+    raceResult[sizeof(raceResult)-1] = '\0';
+
+    // strncat(raceResult, racer1, sizeof(raceResult) - sizeof(raceResult) - 1); // this is incorrect because sizeof refers to the entire buffer in the char array, not the size of the current usage
+    strncat(raceResult, racer2, sizeof(raceResult) - strlen(raceResult) - 1);
+
+    std::cout << "The length of the result is: " << strlen(raceResult) << std::endl;
+    std::cout << raceResult << std::endl;
+
+    std::cout << std::endl;
 
     std::cout << std::endl << std::endl;
     return 0;
