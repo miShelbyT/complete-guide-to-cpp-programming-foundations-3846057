66caac78d044e9eff0d68df67450f87ce084bbc0
Shelby Talbert <mi.shelbyrose@gmail.com>
2025-07-17 18:17:35 +0000

    conditional practice

--- src/Ch05/05_01b/CodeDemo.cpp
+++ src/Ch05/05_01b/CodeDemo.cpp
@@ -3,12 +3,30 @@
 // If Statements, by Eduardo Corpeño 
 
 #include <iostream>
+#include <cctype> // Required header for tolower()
 
 int main(){
-    int score = 1023;
-    bool gamePaused = false;
-    char action = 'x';
+    int score = 1025;
+    bool gamePaused = !bool(0);
+    char action = 'X';
+    char lowerChar = std::tolower(action);
+
+    if(score % 2) // result is true and not 0
+        std::cout << "This score: " << score << " is odd" << std::endl;
+    else
+        std::cout << "This score: " << score << " is even" << std::endl;
+
+    std::cout << "The action '" << action << "' is ";
+    if (lowerChar != 'a' && lowerChar != 'w' && lowerChar != 'd' && lowerChar != 's')
+        std::cout << "not ";
+    std::cout << "a movement key." << std::endl;
+
+    if (gamePaused)
+        std::cout << "The game is paused!" << std::endl;
+    else 
+        std::cout << "The game is running!" << std::endl;
     
+        
     std::cout << std::endl << std::endl;
     return 0;
 }
