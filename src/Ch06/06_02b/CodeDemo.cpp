1fda3910c35d9684909d899bdc1d0c3379f1a6d5
Shelby Talbert <mi.shelbyrose@gmail.com>
2025-07-23 15:05:31 +0000

    functions with differing parameter types

--- src/Ch06/06_02b/CodeDemo.cpp
+++ src/Ch06/06_02b/CodeDemo.cpp
@@ -4,13 +4,32 @@
 
 #include <iostream>
 
+// pass by value
+int square(int x){
+    return x * x;
+}
+
+// args as pointers
+void swap(int *x, int *y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+// args as references. we can overload the previous function with the same signature
+void swap(int& x, int& y) {
+    int temp = x;
+    x = y;
+    y = temp;
+}
+
 int main(){
     int a = 9, b;
-    // TODO: square
+    b = square(a);
     std::cout << "a = " << a << ", b = " << b << std::endl;
-    // TODO: swap
+    swap(&a, &b);
     std::cout << "a = " << a << ", b = " << b << std::endl;
-    // TODO: swap
+    swap(a, b);
     std::cout << "a = " << a << ", b = " << b << std::endl;
     
     std::cout << std::endl << std::endl;
