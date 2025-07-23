75dbbaeb2a451ef8d57da708b420b44f575a3a4c
Shelby Talbert <mi.shelbyrose@gmail.com>
2025-07-23 21:43:20 +0000

    method overloading

--- src/Ch06/06_05b/CodeDemo.cpp
+++ src/Ch06/06_05b/CodeDemo.cpp
@@ -10,11 +10,29 @@ int getSpeed(int distance, int time){
     return distance / time;
 }
 
+int getSpeed(double time) {
+    return static_cast<int>(100 / time);
+}
+// once again, referencing the unit instead of inserting the variable keeps the system from copying the variable into memory
+std::string getSpeed(int distance, int time, const std::string& unit) {
+    int quotient = getSpeed(distance, time);
+    return std::to_string(quotient) + " " + unit;
+}
+
 int main(){
     int speedInt;
+    std::string speedStr;
 
     speedInt = getSpeed(200, 4);
     std::cout << "Speed (int): " << speedInt << std::endl;
+
+    speedInt = getSpeed(3.5);
+    std::cout << "Speed (default distance with time of 3.5): " << speedInt << std::endl;
+
+    speedStr = getSpeed(277, 6, "meters per second");
+
+    std::cout << "Speed (f string): " << speedStr << std::endl;
+
     
     std::cout << std::endl << std::endl;
     return 0;
