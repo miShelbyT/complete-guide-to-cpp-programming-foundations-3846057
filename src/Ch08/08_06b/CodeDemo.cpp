29b6f2ddcb6432c2e3aeff0028e8bd8abcc219fe
Shelby Talbert <mi.shelbyrose@gmail.com>
2025-08-18 20:59:50 +0000

    using stacks and queues

--- src/Ch08/08_06b/CodeDemo.cpp
+++ src/Ch08/08_06b/CodeDemo.cpp
@@ -4,9 +4,11 @@
 
 #include <iostream>
 #include <queue>
+#include <stack>
 
 int main(){
     std::queue<std::string> eventQueue;  // FIFO container for game events
+    std::stack<std::string> undoStack;   // LIFO for undo operations
 
     // Adding events to the queue
     eventQueue.push("Move Forward");
@@ -17,8 +19,37 @@ int main(){
     while (!eventQueue.empty()){
         std::string currentEvent = eventQueue.front();
         std::cout << "Performing event: " << currentEvent << std::endl;
+
+        undoStack.push(currentEvent);
         eventQueue.pop(); 
     }
+
+    std::cout << "Queue is empty" << std::endl;
+
+    std::cout << "Undo stack list ================" << std::endl;
+
+    // Processing events in LIFO order
+    while(!undoStack.empty()) {
+        std::cout << "Undo event: " << undoStack.top() << std::endl;
+        undoStack.pop();
+    }
+
+    std::cout << "Stack is empty" << std::endl;
+
+
+
+    std::cout << "PQ ================" << std::endl;
+
+    std::priority_queue<int> aiTasks;
+    aiTasks.push(1); // Low priority task
+    aiTasks.push(3); // High priority task
+    aiTasks.push(2); // Medium priority task
+
+    while (!aiTasks.empty()){
+        std::cout << "Processing task with priority: " << aiTasks.top() << std::endl;
+        aiTasks.pop();
+    }
+
 
     std::cout << std::endl << std::endl;
     return 0;
